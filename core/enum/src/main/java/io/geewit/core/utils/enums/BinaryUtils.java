bb9004082e507bbdbf04f152dba7b385b11f1934
geewit <hf@geewit.io>
2020-02-02 20:46:14 +0800

    init

--- core/enum/src/main/java/io/geewit/core/utils/enums/BinaryUtils.java
+++ core/enum/src/main/java/io/geewit/core/utils/enums/BinaryUtils.java
@@ -1,5 +1,7 @@
 package io.geewit.core.utils.enums;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.EnumSet;
 import java.util.stream.Collectors;
 
@@ -32,13 +34,24 @@ public class BinaryUtils {
      */
     @SafeVarargs
     public static <E extends Enum<E>> int toBinary(E... enumTypes) {
-        int value = 0;
-        if(enumTypes != null && enumTypes.length > 0) {
-            for (E enumType : enumTypes) {
-                value |= 1 << enumType.ordinal();
-            }
+        if(enumTypes == null || enumTypes.length == 0) {
+            return 0;
         }
-        return value;
+        return Arrays.stream(enumTypes).mapToInt(enumType -> 1 << enumType.ordinal()).reduce(0, (a, b) -> a | b);
+    }
+
+    /**
+     * 打开对应枚举集合的二进制值
+     *
+     * @param enumSet 枚举集合
+     * @param <E>     枚举
+     * @return 二进制值
+     */
+    public static <E extends Enum<E>> int toBinary(Collection<E> enumSet) {
+        if (null == enumSet) {
+            return 0;
+        }
+        return enumSet.stream().mapToInt(enu -> 1 << enu.ordinal()).reduce(0, (a, b) -> a | b);
     }
 
     /**
@@ -49,5 +62,52 @@ public class BinaryUtils {
      */
     public static <E extends Enum<E>> EnumSet<E> fromBinary(int binary, Class<E> clazz) {
         return EnumSet.allOf(clazz).stream().filter(e -> ((binary & 1 << e.ordinal()) > 0)).collect(Collectors.toCollection(() -> EnumSet.noneOf(clazz)));
+    }
+
+
+    /**
+     * 打开对应枚举类所有开关的二进制值
+     *
+     * @param clazz 枚举类
+     * @param <E>   枚举
+     * @return 二进制值
+     */
+    public static <E extends Enum<E>> int allTrue(Class<E> clazz) {
+        return EnumSet.allOf(clazz).stream().mapToInt(enu -> (1 << enu.ordinal())).reduce(0, (a, b) -> a | b);
+    }
+
+
+
+    /**
+     * 检查二进制参数的枚举开关
+     *
+     * @param enu   枚举开关
+     * @param value 二进制参数
+     * @param <E>   枚举
+     * @return true:开|false:关
+     */
+    public static <E extends Enum<E>> boolean is(E enu, int value) {
+        return (1 << enu.ordinal() & value) > 0;
+    }
+
+    /**
+     * 检查二进制参数的枚举开关集合是否全开
+     *
+     * @param enumSet 枚举开关集合
+     * @param value   二进制参数
+     * @param <E>     枚举
+     * @return true: 全开
+     */
+    public static <E extends Enum<E>> boolean hasAll(Collection<E> enumSet, int value) {
+        if (null != enumSet) {
+            for (E enu : enumSet) {
+                if ((1 << enu.ordinal() & value) == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- core/enum/src/main/java/io/geewit/core/utils/enums/EnumMapUtils.java
+++ core/enum/src/main/java/io/geewit/core/utils/enums/EnumMapUtils.java
@@ -1,7 +1,6 @@
 package io.geewit.core.utils.enums;
 
 
-import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,64 +37,6 @@ public class EnumMapUtils {
      */
     public static <E extends Enum<E>> Map<E, Boolean> toEnumMap(Class<E> clazz, int value) {
         return EnumSet.allOf(clazz).stream().collect(Collectors.toMap(enu -> enu, enu -> (value & (1 << enu.ordinal())) > 0, (a, b) -> b));
-    }
-
-    /**
-     * 检查二进制参数的枚举开关
-     *
-     * @param enu   枚举开关
-     * @param value 二进制参数
-     * @param <E>   枚举
-     * @return true:开|false:关
-     */
-    public static <E extends Enum<E>> boolean is(E enu, int value) {
-        return (1 << enu.ordinal() & value) > 0;
-    }
-
-    /**
-     * 检查二进制参数的枚举开关集合是否全开
-     *
-     * @param enumSet 枚举开关集合
-     * @param value   二进制参数
-     * @param <E>     枚举
-     * @return true: 全开
-     */
-    public static <E extends Enum<E>> boolean hasAll(Collection<E> enumSet, int value) {
-        if (null != enumSet) {
-            for (E enu : enumSet) {
-                if ((1 << enu.ordinal() & value) == 0) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * 打开对应枚举集合的二进制值
-     *
-     * @param enumSet 枚举集合
-     * @param <E>     枚举
-     * @return 二进制值
-     */
-    public static <E extends Enum<E>> int toBinary(Collection<E> enumSet) {
-        if (null == enumSet) {
-            return 0;
-        }
-        return enumSet.stream().mapToInt(enu -> 1 << enu.ordinal()).reduce(0, (a, b) -> a | b);
-    }
-
-    /**
-     * 打开对应枚举类所有开关的二进制值
-     *
-     * @param clazz 枚举类
-     * @param <E>   枚举
-     * @return 二进制值
-     */
-    public static <E extends Enum<E>> int allTrue(Class<E> clazz) {
-        return EnumSet.allOf(clazz).stream().mapToInt(enu -> (1 << enu.ordinal())).reduce(0, (a, b) -> a | b);
     }
 
     /**
