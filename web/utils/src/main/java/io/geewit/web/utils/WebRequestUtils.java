8528deff319d79c759906131779bf5424a0de638
geewit <hf@geewit.io>
2020-06-16 17:54:29 +0800

    init

--- web/utils/src/main/java/io/geewit/web/utils/WebRequestUtils.java
+++ web/utils/src/main/java/io/geewit/web/utils/WebRequestUtils.java
@@ -2,12 +2,11 @@ package io.geewit.web.utils;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -26,20 +25,31 @@ public class WebRequestUtils {
         return (userAgent.indexOf("msie") > 0 || userAgent.indexOf("rv:11.0") > 0 || userAgent.indexOf("edge") > 0);
     }
 
-    public static Map<String, Cookie> getCookies(HttpServletRequest request, String... names) {
+    public static Map<String, String> getCookies(HttpServletRequest request, String... names) {
         Assert.notNull(request, "Request must not be null");
         Cookie[] cookies = request.getCookies();
-        Map<String, Cookie> result = new HashMap<>();
+        Map<String, String> result = new HashMap<>();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 for(String name : names) {
-                    if (name.equals(cookie.getName())) {
-                        result.put(name, cookie);
+                    if (name.equals(cookie.getName()) && !StringUtils.isEmpty(cookie.getValue())) {
+                        result.put(name, cookie.getValue());
                     }
                 }
 
             }
         }
+        return result;
+    }
+
+    public static Map<String, Enumeration<String>> getHeaders(HttpServletRequest request, String... names) {
+        Assert.notNull(request, "Request must not be null");
+        Map<String, Enumeration<String>> result = new HashMap<>();
+        for(String name : names) {
+            Enumeration<String> headers = request.getHeaders(name);
+            result.put(name, headers);
+        }
+
         return result;
     }
 }
