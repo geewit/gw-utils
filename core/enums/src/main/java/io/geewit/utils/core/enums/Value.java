26304b82626f8e5a0af64520e24bb85d4cc75818
geewit <hf@geewit.io>
2025-12-05 18:12:34 +0800

    feat(core): 为枚举值接口添加泛型支持
    - 在 Value 接口中添加泛型参数 V 继承自 Number
    - 更新接口文档注释以反映泛型变化
    - 为 value() 方法添加泛型返回类型声明

--- core/enums/src/main/java/io/geewit/utils/core/enums/Value.java
+++ core/enums/src/main/java/io/geewit/utils/core/enums/Value.java
@@ -3,10 +3,12 @@ package io.geewit.utils.core.enums;
 /**
  * 枚举可以实现该接口
  * @author geewit
+ * @param <V> Number 类型
  */
 @SuppressWarnings({"unused"})
 public interface Value<V extends Number> {
     /**
+     * 获取数字
      * @return 数字
      */
     V value();
